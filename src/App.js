8bab34cac403111bd6f77097b8bce1c2c5709956
annapurna <mallickannapurna3@gmail.com>
2023-11-25 13:16:58 +0530

    adding herosection

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg'
 import './App.css';
 import Navbar from './Components/Navbar';
-
+import HeroSection from './Components/HeroSection';
 function App() {
   return ( 
+    <>
     <Navbar />
-  )
-    
-  
+    <HeroSection />
+    </>
+  ) 
 }
 
 export default App;
---
+++ src/Components/HeroSection/index.jsx
@@ -0,0 +1,15 @@
+import './Herosection.css';
+
+export default () => {
+    return <div className="hs-container">
+        <div className="hs-content">
+            <div>
+                <h1>100 Thousand Songs, ad-free</h1>
+                <h1>Over Thousands podcast episodes</h1>
+            </div>
+            <div className="hs-img-frame">
+                <img src='vibrating-headphone 1.png' className="hs-img" />
+            </div>
+        </div>
+    </div>
+}
