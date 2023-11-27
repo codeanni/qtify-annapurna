b4f51f2f407119c08edea5039edbe226d9280bbc
annapurna <mallickannapurna3@gmail.com>
2023-11-27 19:01:48 +0530

    grid-qtify-takehome

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,19 +1,23 @@
 import './Card.css';
 
-export default () => {
+export default ({
+    imgSrc,
+    followersCount,
+    label
+}) => {
     return (<div className='card-wrapper'>
         <div className='card'>
             <div className='card-img-frame'>
-                <img className='card-img' src="https://images.pexels.com/photos/10049264/pexels-photo-10049264.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=1200&w=800" />
+                <img className='card-img' src={imgSrc} />
             </div>
             <div className='card-content'>
                 <span className='card-content-pill'>
-                    100 follows
+                    {followersCount} follows
                 </span>
             </div>
 
         </div>
-        <p className='card-lebel'>New English Songs</p>
+        <p className='card-lebel'>{label}</p>
 
         </div>
         
