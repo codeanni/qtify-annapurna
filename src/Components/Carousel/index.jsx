c2fa3ab213847d33910219933a13ce30625548b6
annapurna <mallickannapurna3@gmail.com>
2023-12-02 13:43:52 +0530

    cardCarouselTop&NewAlbums-qtify-takehome

---
+++ src/Components/Carousel/index.jsx
@@ -0,0 +1,39 @@
+import Card from '../Card';
+import { Virtual, Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import './Carousel.css';
+export default ({data}) => {
+    return <div className='carousel-container'>
+        <Swiper
+        modules={[Virtual, Navigation, Pagination]}
+        // onSwiper={setSwiperRef}
+        slidesPerView={7}
+        // centeredSlides={true}
+        spaceBetween={30}
+        // pagination={{
+        //   type: 'fraction',
+        // }}
+        // navigation={true}
+        navigation={{ nextEl: ".arrow-left", prevEl: ".arrow-right" }}
+        virtual
+      >
+        {data.map(cardData => <SwiperSlide key={cardData.id}><Card
+            imgSrc={cardData.image}
+            followersCount={cardData.follows}
+            label={cardData.title}
+            /></SwiperSlide>)}
+        
+      </Swiper>
+
+      <div className="arrow-left arrow"><img src="/lefticon.png" /></div>
+      <div className="arrow-right arrow"><img src="/righticon.png" /></div>
+
+      </div>
+
+}
--- src/Components/Section/index.js
+++ src/Components/Section/index.js
@@ -1,16 +1,29 @@
 import './Section.css';
 import Card from '../Card';
+import { useState } from 'react';
+import Carousel from '../Carousel';
 
 export default ({ title, data }) => {
+
+    const [isCollapsed, setIsCollapsed] = useState(false);
+
     return <div className='section'>
+
+        <div className='section-header'>
         <h1 className='title'>{title}</h1>
-        <div className='card-container'>
+        <h1 className='section-toggle-btn'
+        onClick={() => {
+            setIsCollapsed(!isCollapsed)
+        }}>{isCollapsed ? 'Show All' : 'Collapse'}</h1>
+        </div>
+        
+        {isCollapsed ? <Carousel data={data}/> : <div className='card-container'>
             {data.map(cardData => <Card
             key={cardData.id}
             imgSrc={cardData.image}
             followersCount={cardData.follows}
             label={cardData.title}
             />)}
-        </div>
+        </div>}
     </div>
 }
